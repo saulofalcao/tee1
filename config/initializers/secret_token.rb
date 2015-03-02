b3e8836d369db4c9af5a8943b8ad5fb2a9d0ff61
Saulo Falcão - Casa <saulofalcao@gmail.com>
2015-03-02 09:01:08 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tee::Application.config.secret_key_base = 'a6f6209a9cd51ae6eb824f7af4249dc21329f08680ad747544aeabdade1e147a001f06dd59fa43394d10162904d7de8fe7636527a0311efc20905b56fa053dd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tee::Application.config.session_store :cookie_store, key: '_tee_session'
